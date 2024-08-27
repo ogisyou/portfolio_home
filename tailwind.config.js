bc4cfc54f98914c3192c5053853643b421fee9db
OS <ogisyou1249605@gmail.com>
2024-08-27 14:24:52 +0900

    fv:tailwind修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        noto: ['"Noto Serif JP"', 'serif'], // Google Fontsで指定したフォントファミリーを追加
+      },
+    },
   },
   plugins: [],
 }
